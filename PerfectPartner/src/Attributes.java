566bbd61a267c5e28228042bf701b0378991e9e5
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-09-24 10:52:39 -0600

    practice objects

---
+++ PerfectPartner/src/Attributes.java
@@ -0,0 +1,26 @@
+
+public class Attributes
+	{
+
+		String name;
+		String job;
+		int age;
+		double height;
+		double weight;
+		boolean isCute;
+		boolean isSmart;
+		
+		
+		public Attributes (String n, String j, int a, double h, double w, boolean c, boolean s)
+		{
+			name = n;
+			job= j;
+		    age = a;
+		    height = h; 
+		    weight = w;
+		    isCute = c;
+		    isSmart = s;
+		}
+		
+		
+	}
---
+++ PerfectPartner/src/Partner.java
@@ -0,0 +1,16 @@
+
+public class Partner
+	{
+		public static void main(String[] args)
+			{
+		
+				System.out.println("");
+				
+				
+				
+				
+			}
+
+		
+		
+	}
